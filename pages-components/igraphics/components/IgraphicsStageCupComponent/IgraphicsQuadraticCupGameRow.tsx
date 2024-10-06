4ee97fdbe21115689549cc9eb3389fee53248a74
fedor <f.avetisov@gmail.com>
2024-10-06 21:44:00 +0300

    changed stage score width

--- pages-components/igraphics/components/IgraphicsStageCupComponent/IgraphicsQuadraticCupGameRow.tsx
+++ pages-components/igraphics/components/IgraphicsStageCupComponent/IgraphicsQuadraticCupGameRow.tsx
@@ -1,38 +1,67 @@
-import s from '@/pages-components/igraphics/components/IgraphicsStageCupComponent/IgraphicsQuadraticCupGame.module.scss';
-import { IgrTeamLogo } from '@/pages-components/igraphics/components/shared-components/IgrTeamLogo';
-import { forwardRef, useEffect, useState } from 'react';
+import s from "@/pages-components/igraphics/components/IgraphicsStageCupComponent/IgraphicsQuadraticCupGame.module.scss";
+import { IgrTeamLogo } from "@/pages-components/igraphics/components/shared-components/IgrTeamLogo";
+import { forwardRef, useEffect, useState } from "react";
 
-export const IgraphicsQuadraticCupGameRow = forwardRef(({ game, schema, teamLogo, teamName, teamScore, team }: any, ref) => {
-  const [name, setName] = useState('');
-  const [scoreResult, setScoreResult] = useState([]);
+export const IgraphicsQuadraticCupGameRow = forwardRef(
+  ({ game, schema, teamLogo, teamName, teamScore, team }: any, ref) => {
+    const [name, setName] = useState("");
+    const [scoreResult, setScoreResult] = useState([]);
 
-  useEffect(() => {
-    setName(teamName);
-    setScoreResult(teamScore);
-  }, [team]);
+    useEffect(() => {
+      setName(teamName);
+      setScoreResult(teamScore);
+    }, [team]);
 
-  const onChangeScore = (value, score, idx, type: 'pen' | 'ft') => {
-    let prevScore = scoreResult;
-    prevScore[idx][type] = value;
-    setScoreResult([...prevScore]);
-  };
+    const onChangeScore = (value, score, idx, type: "pen" | "ft") => {
+      let prevScore = scoreResult;
+      prevScore[idx][type] = value;
+      setScoreResult([...prevScore]);
+    };
 
-  if (!team) {
-    return <div className={s.team} style={{ backgroundColor: schema.colors.rowOdd }} />;
-  }
+    if (!team) {
+      return (
+        <div
+          className={s.team}
+          style={{ backgroundColor: schema.colors.rowOdd }}
+        />
+      );
+    }
 
-  return (
-    <div className={s.team} style={{ backgroundColor: schema.colors.rowOdd }}>
-      <IgrTeamLogo logo={teamLogo} />
-      <div className={s.name}>
-        <input onChange={e => setName(e.target.value)} value={name} style={{ color: schema.colors.rowFont }} />
-      </div>
-      {scoreResult.map((sc, idx) => (
-        <div key={idx} ref={el => el?.style?.setProperty('--borderColor', schema.colors.rowFont)} style={{ color: schema.colors.rowFont }} className={s.score}>
-          <input style={{ color: schema.colors.rowFont }} onChange={e => onChangeScore(e.target.value, sc, idx, 'ft')} value={sc.ft} />
-          {sc.hasPen && <input style={{ color: schema.colors.rowFont }} onChange={e => onChangeScore(e.target.value, sc, idx, 'pen')} value={sc.pen} className={s.scorePen} />}
+    return (
+      <div className={s.team} style={{ backgroundColor: schema.colors.rowOdd }}>
+        <IgrTeamLogo logo={teamLogo} />
+        <div className={s.name}>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            style={{ color: schema.colors.rowFont }}
+          />
         </div>
-      ))}
-    </div>
-  );
-});
+        {scoreResult.map((sc, idx) => (
+          <div
+            key={idx}
+            ref={(el) =>
+              el?.style?.setProperty("--borderColor", schema.colors.rowFont)
+            }
+            style={{ color: schema.colors.rowFont }}
+            className={s.score}
+          >
+            <input
+              style={{ color: schema.colors.rowFont }}
+              onChange={(e) => onChangeScore(e.target.value, sc, idx, "ft")}
+              value={sc.ft}
+            />
+            {sc.hasPen && (
+              <input
+                style={{ color: schema.colors.rowFont }}
+                onChange={(e) => onChangeScore(e.target.value, sc, idx, "pen")}
+                value={sc.pen}
+                className={s.scorePen}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  }
+);
