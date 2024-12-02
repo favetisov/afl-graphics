c067ea73df5dcd13b20b637de8bd2301ac183f5d
fedor <f.avetisov@gmail.com>
2024-12-02 08:42:48 +0300

    added cross button autohide

--- shared/shared-frontend/components/PhotoLoader/PhotoLoader.tsx
+++ shared/shared-frontend/components/PhotoLoader/PhotoLoader.tsx
@@ -1,8 +1,9 @@
-import s from './PhotoLoader.module.scss';
-import { useEffect, useRef, useState } from 'react';
-import { Button, LoadingOverlay } from '@mantine/core';
-import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
-import { X as CloseIcon, Photo as PhotoIcon } from 'tabler-icons-react';
+import s from "./PhotoLoader.module.scss";
+import { useEffect, useRef, useState } from "react";
+import { Button, LoadingOverlay } from "@mantine/core";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { X as CloseIcon, Photo as PhotoIcon } from "tabler-icons-react";
+import { debounce } from "lodash";
 
 const CANVAS_SCALE = 1; // making big canvas to keep quality
 
@@ -20,10 +21,15 @@ export const PhotoLoader = ({
   className?: string;
 }) => {
   const [loading, setLoading] = useState(Boolean(defaultUrl));
-  const [photo, setPhoto] = useState('');
+  const [photo, setPhoto] = useState("");
   const [defaultInitiated, setDefautlInitiated] = useState(false);
   const [showCloseButton, setShowCloseButton] = useState(true);
   const canvasEl = useRef(null);
+
+  const hideCloseButton = () => {
+    setShowCloseButton(false);
+  };
+  const debounceHideCloseButton = debounce(hideCloseButton, 1000);
 
   useEffect(() => {
     if (!defaultInitiated && defaultUrl && canvasEl?.current) {
@@ -31,15 +37,16 @@ export const PhotoLoader = ({
       (async () => {
         const image = new Image();
         image.onload = () => {
-          const { width: canvasWidth, height: canvasHeight } = canvasEl.current.getBoundingClientRect();
+          const { width: canvasWidth, height: canvasHeight } =
+            canvasEl.current.getBoundingClientRect();
           const scale = canvasHeight / image.height;
           const height = image.height * scale;
           const width = image.width * scale;
-          image.crossOrigin = 'anonymous';
-          const canvas = document.createElement('canvas');
-          canvas.setAttribute('width', canvasWidth);
-          canvas.setAttribute('height', canvasHeight);
-          const ctx = canvas.getContext('2d');
+          image.crossOrigin = "anonymous";
+          const canvas = document.createElement("canvas");
+          canvas.setAttribute("width", canvasWidth);
+          canvas.setAttribute("height", canvasHeight);
+          const ctx = canvas.getContext("2d");
           ctx.drawImage(image, 0, 0, width / 1, height / 1);
           setPhoto(canvas.toDataURL());
           setLoading(false);
@@ -49,14 +56,16 @@ export const PhotoLoader = ({
     }
   });
 
-  const drawOnCanvas = e => {
+  const drawOnCanvas = (e) => {
+    setShowCloseButton(true);
     /** looks meaningless, but without this assertions canvas won't scale images properly */
-    const { width: canvasWidth, height: canvasHeight } = canvasEl.current.getBoundingClientRect();
-    canvasEl.current.setAttribute('width', canvasWidth);
-    canvasEl.current.setAttribute('height', canvasHeight);
+    const { width: canvasWidth, height: canvasHeight } =
+      canvasEl.current.getBoundingClientRect();
+    canvasEl.current.setAttribute("width", canvasWidth);
+    canvasEl.current.setAttribute("height", canvasHeight);
     /** ^^ end of hack */
 
-    const context = canvasEl.current.getContext('2d');
+    const context = canvasEl.current.getContext("2d");
     const image = new Image();
     context.clearRect(0, 0, canvasEl.current.width, canvasEl.current.height);
     image.onload = () => {
@@ -66,9 +75,10 @@ export const PhotoLoader = ({
       onImg(canvasEl.current.toDataURL());
     };
     image.src = photo;
+    debounceHideCloseButton();
   };
 
-  const onImgLoad = e => {
+  const onImgLoad = (e) => {
     setLoading(true);
     const fileReader = new FileReader();
     fileReader.onload = (event: any) => {
@@ -77,26 +87,59 @@ export const PhotoLoader = ({
     };
     fileReader.readAsDataURL(e.target.files[0]);
     setLoading(false);
+    setTimeout(() => {
+      setShowCloseButton(false);
+    }, 1000);
   };
 
   return (
-    <div className={`${s.loader} ${className}`} onMouseOver={() => setShowCloseButton(true)} onMouseOut={() => setShowCloseButton(false)}>
+    <div
+      className={`${s.loader} ${className}`}
+      onClick={() => setShowCloseButton(true)}
+      onMouseOver={() => setShowCloseButton(true)}
+      onMouseOut={() => setShowCloseButton(false)}
+    >
       <LoadingOverlay visible={loading} />
       {photo && showCloseButton && (
-        <Button onClick={() => setPhoto(null)} className={s.closeButton + ' photoloader-button'}>
+        <Button
+          onClick={() => setPhoto(null)}
+          className={s.closeButton + " photoloader-button"}
+        >
           <CloseIcon size={14} />
         </Button>
       )}
-      {!photo && <input type={'file'} accept=".jpg, .jpeg, .png" onChange={e => onImgLoad(e)} />}
+      {!photo && (
+        <input
+          type={"file"}
+          accept=".jpg, .jpeg, .png"
+          onChange={(e) => onImgLoad(e)}
+        />
+      )}
       {!photo && <PhotoIcon size={100} className={s.photoIcon} />}
       {photo && (
-        <TransformWrapper onInit={drawOnCanvas} onZoomStop={drawOnCanvas} onPanningStop={drawOnCanvas} minScale={0.1} limitToBounds={false}>
+        <TransformWrapper
+          onInit={drawOnCanvas}
+          onZoomStop={drawOnCanvas}
+          onPanningStop={drawOnCanvas}
+          minScale={0.1}
+          limitToBounds={false}
+        >
           <TransformComponent>
-            <img src={photo} height={height} width={width} style={{ objectFit: 'contain' }} />
+            <img
+              src={photo}
+              height={height}
+              width={width}
+              style={{ objectFit: "contain" }}
+            />
           </TransformComponent>
         </TransformWrapper>
       )}
-      <canvas ref={canvasEl} className={s.canvas} height={height} width={width} />
+      <canvas
+        ref={canvasEl}
+        className={s.canvas}
+        height={height}
+        width={width}
+      />
     </div>
   );
 };
