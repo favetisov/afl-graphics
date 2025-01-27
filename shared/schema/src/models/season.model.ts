5977856d6b8282220de6f8bbf697fe4076f76a75
fedor <f.avetisov@gmail.com>
2025-01-27 08:05:15 +0300

    updated stats calculation

--- shared/schema/src/models/season.model.ts
+++ shared/schema/src/models/season.model.ts
@@ -6,6 +6,7 @@ import { Player } from "./player.model";
 import { Team } from "./team.model";
 import dayjs from "dayjs";
 import { AbstractModel } from "./base/abstract.model";
+import { orderBy } from "lodash";
 
 export class Season extends AbstractModel {
   private startingAt: string;
@@ -53,30 +54,38 @@ export class Season extends AbstractModel {
   };
 
   calculate() {
-    this.playersStats = this.stages.reduce((stats, stage) => {
-      if (!stage.calculated) stage.calculate();
-      for (const pId in stage.calculated.playersStatsMap) {
-        const row = stage.calculated.playersStatsMap[pId];
-        stats[pId] ??= {
-          player: row.player,
-          goals: 0,
-          assists: 0,
-          goals_assists: 0,
-          yellow: 0,
-          red: 0,
-          games: 0,
-          teamsMap: row.teamsMap,
-        };
+    this.playersStats = orderBy(this.stages, ["_id"], ["asc"]).reduce(
+      (stats, stage) => {
+        if (!stage.calculated) stage.calculate();
+        for (const pId in stage.calculated.playersStatsMap) {
+          const row = stage.calculated.playersStatsMap[pId];
 
-        stats[pId].goals += row.goals;
-        stats[pId].assists += row.assists;
-        stats[pId].goals_assists += row.assists + row.goals;
-        stats[pId].yellow += row.yellow;
-        stats[pId].red += row.red;
-        stats[pId].games += row.played;
-        stats[pId].teams = { ...stats[pId].teams, ...row.teamsMap };
-      }
-      return stats;
-    }, {});
+          stats[pId] ??= {
+            player: row.player,
+            goals: 0,
+            assists: 0,
+            goals_assists: 0,
+            yellow: 0,
+            red: 0,
+            games: 0,
+            teamsMap: row.teamsMap,
+          };
+
+          stats[pId].goals += row.goals;
+          stats[pId].assists += row.assists;
+          stats[pId].goals_assists += row.assists + row.goals;
+          // stats[pId].yellow += row.yellow;
+          // stats[pId].red += row.red;
+
+          stats[pId].yellow = row.yellow;
+          stats[pId].red = row.red;
+
+          stats[pId].games += row.played;
+          stats[pId].teams = { ...stats[pId].teams, ...row.teamsMap };
+        }
+        return stats;
+      },
+      {}
+    );
   }
 }
